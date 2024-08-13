7e4a65fa8df566befe56bbb3b6d294a650638095
Aleksey <evel199315@yandex.ru>
2024-08-13 14:11:56 +0500

    init commit

---
+++ src/test/java/ru/panov/workprocesslog/WorkProcessLogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.panov.workprocesslog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkProcessLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
